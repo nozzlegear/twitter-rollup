1508eafe96905254e70307e50b4849a16a3197ce
Joshua Harms <joshua@nozzlegear.com>
2017-03-20 20:53:23 -0500

    Converting token to base64; added debug notes

--- TwitterClient.cs
+++ TwitterClient.cs
@@ -1,21 +1,28 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Flurl.Http;
 using twitter_rollup.Models;
+using System.Text;
 
 namespace twitter_rollup
 {
     class TwitterClient
     {
-        readonly string BaseUrl = "https://api.twitter.com";
+        readonly string BaseUrl = "https://api.twitter.com/1.1";
 
         public TwitterClient(string token)
         {
+            var bytes = Encoding.UTF8.GetBytes(token);
+
             Token = token;
+            Base64Token = Convert.ToBase64String(bytes);
         }
 
         string Token { get; set; }
+
+        string Base64Token { get; set; }
 
         IFlurlClient PrepareRequest(string path)
         {
@@ -36,6 +43,17 @@ namespace twitter_rollup
 
                 var request = api.GetAsync();
                 var response = await request;
+
+#if DEBUG
+
+                Console.WriteLine(api.Url.ToString());
+                Console.WriteLine("With token {0}", Token);
+
+                var s = await request.ReceiveString();
+
+                Console.WriteLine("Response string: {0}", s);
+
+#endif
 
                 response.EnsureSuccessStatusCode();
 
